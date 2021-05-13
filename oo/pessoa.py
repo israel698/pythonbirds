a83d377247ac88367f4c09e95638191bee143ab8
Ubiranilde <israelsouza900@gmail.com>
2021-05-12 21:26:46 -0300

    salvar primeiro pj

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,13 +1,19 @@
 class Pessoa:
-    def __init__(self, nome=None, idade= 35):
+    def __init__(self, *filhos, nome=None, idade= 35):
         self.nome = nome
         self.idade = idade
+        self. filhos = list(filhos)
 
     def cumprimentar(self):
         return 'ola'
 
 if __name__ == '__main__':
-    p = Pessoa('israel')
-    print(p.cumprimentar())
+    f = Pessoa(nome='apollo')
+    p = Pessoa(f, nome='israel')
+    print(Pessoa.cumprimentar(p))
     print(p.nome)
     print(p.idade)
+    for filho in p.filhos:
+        print(filho.nome)
+
+
