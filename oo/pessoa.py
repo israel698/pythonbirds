805941a0761c829560372c2c34a0f20297970e8e
Ubiranilde <israelsouza900@gmail.com>
2021-05-12 21:02:37 -0300

    salvar primeiro pj

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,7 +1,13 @@
 class Pessoa:
+    def __init__(self, nome=None, idade= 35):
+        self.nome = nome
+        self.idade = idade
+
     def cumprimentar(self):
         return 'ola'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('israel')
     print(p.cumprimentar())
+    print(p.nome)
+    print(p.idade)
